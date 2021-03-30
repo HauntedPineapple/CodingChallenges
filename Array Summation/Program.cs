6d5a94efe8622f4728e0b7c0b5ea23319e70a703
Audrey Main <meilong.main@gmail.com>
2021-03-30 13:45:33 -0400

    Created README Wrote code for testing

--- Array Summation/Program.cs
+++ Array Summation/Program.cs
@@ -10,14 +10,48 @@ namespace Array_Summation
         /// first array add up to any number
         /// in the second array and "false" otherwise
         /// </summary>
-        public static bool arraySummation(int[] array_1, int [] array_2)
+        public static string arraySummation(int[] array_1, int[] array_2)
         {
-            return false;
+            return "FALSE";
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array_1, array_2;
+
+            Console.WriteLine("=========== Test Case #1 ===========");
+            array_1 = new int[3] { -1, 8, 3 };
+            array_2 = new int[3] { 3, 7, 2 };
+            Console.Write("Array_1 contains the numbers: ");
+            foreach (int number in array_1)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+            Console.Write("Array_2 contains the numbers: ");
+            foreach (int number in array_2)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Result: " + arraySummation(array_1, array_2));
+
+            Console.WriteLine("=========== Test Case #2 ===========");
+            array_1 = new int[3] { 9, 6, 12 };
+            array_2 = new int[3] { 1, 2, 3 };
+            Console.Write("Array_1 contains the numbers: ");
+            foreach (int number in array_1)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+            Console.Write("Array_2 contains the numbers: ");
+            foreach (int number in array_2)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Result: " + arraySummation(array_1, array_2));
         }
     }
 }
---
+++ Array Summation/README.txt
@@ -0,0 +1,16 @@
+This function takes two intteger arrays as inputs.
+It returns TRUE if any two numbers in the first array add up to any one number
+in the second array. If none of the numbers match this criteria, the function returns FALSE.
+
+Examples:
+	Given:
+	array_1 = {-1, 8, 3}
+	array_2 = {3, 7, 2}
+	returns TRUE because -1 + 3 is 2, also -1 + 8 = 7
+
+	Given:
+	Given:
+	array_1 = {9, 6, 12}
+	array_2 = {1, 2, 3}
+	returns FALSE because no pair of numbers in the first array add up to
+	any of the numbers in the second array
