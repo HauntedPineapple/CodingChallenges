64c0710d3dc4406856c111be0ca0a3ea9b94e97b
Audrey Main <meilong.main@gmail.com>
2021-03-30 13:29:25 -0400

    Created project

---
+++ Username Changes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Username_Changes
+{
+    class Program
+    {
+        /// <summary>
+        /// For each username given, returns either YES or NO based on whether
+        /// that username can be changed with one swap to a new one
+        /// that is smaller in alphabetical order than the original username
+        /// </summary>
+        /// <param name="usernames">List of usernames</param>
+        /// <returns>A list of strings corresponding to each given username that tells
+        /// if the username can be changed with one swap to a new one
+        /// that is smaller in alphabetical order than the original username </returns>
+        public static List<string> possibleChanges(List<string> usernames)
+        {
+            List<string> result = new List<string>();
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
