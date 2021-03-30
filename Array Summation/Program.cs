87494478ecea62ee623d27540b8671559188b6bf
Audrey Main <meilong.main@gmail.com>
2021-03-30 13:34:38 -0400

    Created project

---
+++ Array Summation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_Summation
+{
+    class Program
+    {
+        /// <summary>
+        /// Returns "true" if any two numbers in the
+        /// first array add up to any number
+        /// in the second array and "false" otherwise
+        /// </summary>
+        public static bool arraySummation(int[] array_1, int [] array_2)
+        {
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
