eab25ccf36b3382fd35025ce3f0924403f67b812
Audrey Main <meilong.main@gmail.com>
2021-04-05 20:57:58 -0400

    started part 1

--- EscapeVelocity_C-sharp/Program.cs
+++ EscapeVelocity_C-sharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EscapeVelocity_C_sharp
 {
@@ -14,20 +10,78 @@ namespace EscapeVelocity_C_sharp
         /// </summary>
         const double G = 6.67e-11;
 
+        /// The escape velocity equation is as follows:
+        /// V -> escape velocity
+        /// G -> gravitational constant
+        /// m -> mass of planet
+        /// r -> radius of planet
+        /// V = sqrt(2Gm/r)
+
         static string EscapeVelocity(string planet)
         {
-            String result = "";
-            return "";
+            string result = "\tThe escape velocity in m/s is: ";
+            // mass and radius are in kg and m
+            double mass = 5.976e24; // Earth's mass
+            double radius = 637800; // Earth's equatorial radius
+
+            switch (planet.ToLower())
+            { //multiplies by the planet's value relative to Earth's
+                case "mercury":
+                    mass *= 0.0558;
+                    radius *= 0.383;
+                    break;
+                case "venus":
+                    mass *= 0.815;
+                    radius *= 0.95;
+                    break;
+                case "earth":
+                    break;
+                case "mars":
+                    mass *= 0.107;
+                    radius *= 0.532;
+                    break;
+                case "jupiter":
+                    mass *= 318;
+                    radius *= 11.2;
+                    break;
+                case "saturn":
+                    mass *= 95.1;
+                    radius *= 9.41;
+                    break;
+                case "uranus":
+                    mass *= 14.5;
+                    radius *= 4.06;
+                    break;
+                case "neptune":
+                    mass *= 17.2;
+                    radius *= 3.88;
+                    break;
+                default:
+                    return "ERROR: Invalid input!";
+            }
+
+            double escVel = 2 * G * mass;
+            escVel = escVel / radius;
+            escVel = Math.Sqrt(escVel);
+            result += decimal.Round((decimal)escVel, 2);
+
+            result += "\n\tThe escape velocity in km/h is : ";
+            result += "\n\tThe escape velocity in km/s is : ";
+            return result;
         }
 
         static string SystemEscapeVelocity(string planet)
         {
-            String result = "";
+            string result = "";
             return "";
         }
 
         static void Main(string[] args)
         {
+            string planet = "Earth";
+            Console.WriteLine(EscapeVelocity(planet));
+
+            Console.ReadLine();
         }
     }
 }
