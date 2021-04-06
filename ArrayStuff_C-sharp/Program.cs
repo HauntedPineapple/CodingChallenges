6d53d98ad360baadc4c5b4230f938ad2a227646a
Audrey Main <meilong.main@gmail.com>
2021-04-06 13:05:29 -0400

    Finished Array Summation

--- ArrayStuff_C-sharp/Program.cs
+++ ArrayStuff_C-sharp/Program.cs
@@ -10,20 +10,46 @@ namespace ArrayStuff_C_sharp
     {
         static void Main(string[] args)
         {
-            testArraySummation();
+            TestArraySummation();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
         }
 
+        #region Diamond Shaped Array
+        #endregion
+
+        #region Multiplication Tables
+        #endregion
+
+        #region Array of Multiples
+        #endregion
+
+        #region Array Summation
         /// <summary>
         /// Returns "true" if any two numbers in the
         /// first array add up to any number
         /// in the second array and "false" otherwise
         /// </summary>
-        static bool arraySummation(int[] array_1, int[] array_2)
+        static bool ArraySummation(int[] array_1, int[] array_2)
         {
+            for (int i = 0; i < array_1.Length - 1; i++)
+            {
+                for (int k = 1; k < array_1.Length; k++)
+                {
+                    foreach (int number in array_2)
+                    {
+                        if (array_1[i] + array_1[k] == number)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
             return false;
         }
-
-        static void testArraySummation()
+        static void TestArraySummation()
         {
             int[] array_1, array_2;
 
@@ -41,8 +67,8 @@ namespace ArrayStuff_C_sharp
             {
                 Console.Write(number + " ");
             }
-            Console.WriteLine("");
-            Console.WriteLine("Result: " + arraySummation(array_1, array_2).ToString());
+            Console.WriteLine();
+            Console.WriteLine("Result: " + ArraySummation(array_1, array_2).ToString());
 
             Console.WriteLine("=========== Test Case #2 ===========");
             array_1 = new int[3] { 9, 6, 12 };
@@ -52,15 +78,24 @@ namespace ArrayStuff_C_sharp
             {
                 Console.Write(number + " ");
             }
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.Write("Array_2 contains the numbers: ");
             foreach (int number in array_2)
             {
                 Console.Write(number + " ");
             }
             Console.WriteLine("");
-            Console.WriteLine("Result: " + arraySummation(array_1, array_2).ToString());
+            Console.WriteLine("Result: " + ArraySummation(array_1, array_2).ToString());
         }
+        #endregion
 
+        #region Missing Integer
+        #endregion
+
+        #region Mini Peaks
+        #endregion
+
+        #region Left Rotation
+        #endregion
     }
 }
