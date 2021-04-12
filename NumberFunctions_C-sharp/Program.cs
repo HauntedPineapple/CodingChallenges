9d38bef9106601207fd062f10f98c4b8698a2f25
Audrey Main <meilong.main@gmail.com>
2021-04-12 14:21:59 -0400

    Wrote more test cases

--- NumberFunctions_C-sharp/Program.cs
+++ NumberFunctions_C-sharp/Program.cs
@@ -10,6 +10,8 @@ namespace NumberFunctions_C_sharp
     {
         static void Main(string[] args)
         {
+            int[] input;
+
             #region Prime
             Console.WriteLine("----- Is It Prime? -----");
             Console.WriteLine("The first 50 prime numbers are: ");
@@ -25,12 +27,12 @@ namespace NumberFunctions_C_sharp
                 number++;
             }
             Console.WriteLine();
-#endregion
+            #endregion
 
             #region Interprime
             Console.WriteLine("----- Interprime Numbers -----");
-            int[] testNums = new int[3] { 6, 9, 13 };
-            foreach (int num in testNums)
+            input = new int[3] { 6, 9, 13 };
+            foreach (int num in input)
             {
                 int[] output = interprime(num);
                 Console.Write("interprime(" + num + ") --> [");
@@ -38,7 +40,7 @@ namespace NumberFunctions_C_sharp
                 {
                     if (i < output.Length - 1)
                     {
-                        Console.Write(output[i]+", ");
+                        Console.Write(output[i] + ", ");
                     }
                     else
                     {
@@ -52,6 +54,11 @@ namespace NumberFunctions_C_sharp
 
             #region Exapnded Form
             Console.WriteLine("----- Expanded Numbers -----");
+            input = new int[3] { 25, 70701, 685 };
+            foreach (int num in input)
+            {
+                Console.WriteLine(num + " --> "+expandedForm(num));
+            }
             #endregion
 
             #region Consecutive Numbers
@@ -60,6 +67,22 @@ namespace NumberFunctions_C_sharp
 
             #region Moran Numbers
             Console.WriteLine("----- Moran Numbers -----");
+            input = new int[3] { 132, 133, 134 };
+            foreach (int num in input)
+            {
+                if (moranOrHarshad(num) == "Neither")
+                {
+                    Console.WriteLine(num + " neither a Moran nor a Harshad number");
+                }
+                if (moranOrHarshad(num) == "M")
+                {
+                    Console.WriteLine(num + " is a Moran number");
+                }
+                if (moranOrHarshad(num) == "H")
+                {
+                    Console.WriteLine(num + " is a Harshad number");
+                }
+            }
             #endregion
 
             #region Fibonacci
@@ -72,10 +95,34 @@ namespace NumberFunctions_C_sharp
 
             #region Perfect Numbers
             Console.WriteLine("----- Perfect Numbers -----");
+            input = new int[3] { 6, 11, 28 };
+            foreach (int num in input)
+            {
+                if (isPerfect(num))
+                {
+                    Console.WriteLine(num + " IS a perfect number");
+                }
+                else
+                {
+                    Console.WriteLine(num + " is NOT a perfect number");
+                }
+            }
             #endregion
 
             #region Palindromic Numbers
             Console.WriteLine("----- Palindromic Numbers -----");
+            input = new int[3] { 123, 36863, 22123 };
+            foreach (int num in input)
+            {
+                if (isPalindrome(num))
+                {
+                    Console.WriteLine(num + " IS a palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(num + " is NOT a palindrome");
+                }
+            }
             #endregion
 
             Console.WriteLine();
@@ -117,6 +164,24 @@ namespace NumberFunctions_C_sharp
             return new int[2] { leftPrime, rightPrime };
         }
 
+        static string expandedForm(int number)
+        {
+            return "";
+        }
+
+        static bool consecutiveNumbers(int[] numbers)
+        {
+            return false;
+        }
+
+        /// <returns>"M" if the number is a Moran number,
+        /// "H" if the number is a Harshad number or "Neither" if it is neither
+        /// </returns>
+        static string moranOrHarshad(int num)
+        {
+            return "Neither";
+        }
+
         static string nthFibonacci(int n)
         {
             if (n < 2)
@@ -140,6 +205,14 @@ namespace NumberFunctions_C_sharp
             return fibNum.ToString();
         }
 
+        static bool isPerfect(int num)
+        {
+            return false;
+        }
 
+        static bool isPalindrome(int num)
+        {
+            return false;
+        }
     }
 }
