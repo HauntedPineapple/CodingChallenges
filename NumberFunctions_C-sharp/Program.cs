c16c3c104f286570f0a05d413eb2c0fb26f5f52b
Audrey Main <meilong.main@gmail.com>
2021-04-12 13:18:15 -0400

    Finished nth Fibonacci

--- NumberFunctions_C-sharp/Program.cs
+++ NumberFunctions_C-sharp/Program.cs
@@ -10,6 +10,28 @@ namespace NumberFunctions_C_sharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static string nthFibonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n.ToString();
+            }
+            int twoBack = 0;
+            int oneBack = 1;
+            int current = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                current = twoBack + oneBack;
+                twoBack = oneBack;
+                oneBack = current;
+            }
+
+            return current.ToString();
         }
     }
 }
