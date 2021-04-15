98e51357e238456965e5fb97868a625b4019a490
Audrey Main <meilong.main@gmail.com>
2021-04-14 20:23:36 -0400

    Created structure for singly linked list

---
+++ LinkedListStuff_C-sharp/CustomLinkedLists/CustomLinkedList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedLists
+{
+    class CustomLinkedList<T>
+    {
+        private int count;
+        private CustomLinkedNode<T> head;
+        private CustomLinkedNode<T> tail;
+
+        public int Count { get { return count; } }
+
+        public T this[int index] //indexer
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException("Invalid index");
+                }
+                CustomLinkedNode<T> current = head;
+                for (int a = 0; a < count - 1; a++)
+                { // loop until we're at the index
+                    current = current.Next;
+                }
+                return current.Data;
+            }
+        }
+
+        public CustomLinkedList()
+        {
+            count = 0;
+            head = null;
+            tail = null;
+        }
+
+        public void Add(T data)
+        {
+            CustomLinkedNode<T> n = new CustomLinkedNode<T>(data);
+            // determine if the list is empty
+            // or has a bunch of nodes already
+            if (count == 0)
+            {
+                head = n;
+                tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+            count++;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("ERROR: Invalid index specified. No object exists for index " + index + "!");
+            }
+            if (index == 0)
+            {
+                head = head.Next;
+            }
+            if (index == count - 1)
+            {
+                tail = GetNode(count - 2);
+            }
+            if (count == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                CustomLinkedNode<T> before = GetNode(index - 1);
+                before.Next = before.Next.Next;
+                count--;
+            }
+        }
+
+        public T GetData(int index)
+        {
+            CustomLinkedNode<T> current = head;
+            for (int a = 0; a < index; a++)
+            {
+                current = current.Next;
+            }
+            return current.Data;
+        }
+
+        public CustomLinkedNode<T> GetNode(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+            CustomLinkedNode<T> current = head;
+            for (int a = 0; a < count - 1; a++)
+            { // loop until we're at the index
+                current = current.Next;
+            }
+            return current;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+    }
+}
---
+++ LinkedListStuff_C-sharp/CustomLinkedLists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList<string> myList = new CustomLinkedList<string>();
+            myList.Add("Red");
+            myList.Add("Blue");
+            myList.Add("Green");
+            myList.Add("Orange");
+            myList.Add("Yellow");
+            myList.Add("White");
+
+            Console.WriteLine(">> Printing Contents:");
+            for (int a = 0; a < myList.Count; a++)
+            {
+                Console.WriteLine(myList.GetData(a));
+            }
+
+            Console.WriteLine(">> Removing items");
+            try
+            {
+                myList.Remove(0);
+                myList.Remove(3);
+                myList.Remove(5);
+                myList.Remove(6);
+            }
+            catch (IndexOutOfRangeException i)
+            {
+                Console.WriteLine(i.Message);
+            }
+
+            Console.WriteLine(">> Printing Contents After Removal:");
+            for (int a = 0; a < myList.Count; a++)
+            {
+                Console.WriteLine(myList.GetData(a));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
