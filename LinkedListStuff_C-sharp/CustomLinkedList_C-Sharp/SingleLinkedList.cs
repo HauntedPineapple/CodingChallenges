5344124d30b02822faf1282cbfe46cbe3ed219ea
Audrey Main <meilong.main@gmail.com>
2021-04-28 13:26:27 -0400

    Started single linked list and node class

---
+++ LinkedListStuff_C-sharp/CustomLinkedList_C-Sharp/SingleLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList_C_Sharp
+{
+    class SingleLinkedList<T>
+    {
+        private int count;
+        private SingleLinkedNode<T> head, tail;
+
+        public SingleLinkedList()
+        {
+
+        }
+
+        void Add(T data)
+        {
+
+        }
+
+        T GetData(int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return head.Data;
+        }
+    }
+}
---
+++ LinkedListStuff_C-sharp/CustomLinkedList_C-Sharp/SingleLinkedNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList_C_Sharp
+{
+    class SingleLinkedNode<T>
+    {
+        private T data;
+        private SingleLinkedNode<T> next;
+
+        public SingleLinkedNode(T data)
+        {
+            this.data = data;
+            next = null;
+        }
+        public SingleLinkedNode(T data, SingleLinkedNode<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public T Data { get { return data; } }
+        public SingleLinkedNode<T> Next { get { return next; } }
+    }
+}
